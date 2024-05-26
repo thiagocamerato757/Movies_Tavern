fd9e36c8ce1cd2fc3aa3b8fabb7463b3dbe1e8b5
Thiago Camerato <104781163+thiagocamerato757@users.noreply.github.com>
2024-05-26 16:00:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('TelaInicial.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
