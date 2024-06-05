a69afde1eceb36e2fe5a4dfee06f09792df072e1
Ana Luiza <aninhapmarques03@gmail.com>
2024-06-05 00:36:33 -0300

    botoes de avaliar e excluir estrelas

--- static/rating.js
+++ static/rating.js
@@ -1,22 +1,56 @@
-var stars = document.querySelectorAll('.star-icon');
-var submitButton = document.getElementById('submit-rating');
+document.addEventListener('DOMContentLoaded', function() {
+    var btnAvaliar = document.getElementById('btn-avaliar');
+    var avaliacao = document.querySelector('.avaliacao');
+    var submitButton = document.getElementById('submit-rating');
+    var stars = document.querySelectorAll('.star-icon');
 
-document.addEventListener('click', function(e) {
-    var classStar = e.target.classList;
-    if (!classStar.contains('ativo')) {
-        stars.forEach(function(star) {
-            star.classList.remove('ativo');
+    btnAvaliar.addEventListener('click', function() {
+        if (btnAvaliar.textContent === 'Excluir') {
+            rating = 0;
+            paintStars(rating);
+            submitButton.style.display = 'none';
+            avaliacao.classList.remove('avaliacao-enviada');
+            btnAvaliar.textContent = 'Avaliar';
+            avaliacao.classList.add('desabilitada');
+        }
+        else{
+            avaliacao.classList.remove('desabilitada');
+            stars.forEach(star => {
+                star.style.pointerEvents = 'auto';
+            });
+        }
+        
+    });
+
+    stars.forEach(star => {
+        star.addEventListener('click', function() {
+            if (!avaliacao.classList.contains('avaliacao-enviada')) {
+                rating = parseInt(star.getAttribute('data-avaliacao'));
+                paintStars(rating);
+                submitButton.style.display = 'block';
+            }
         });
-        classStar.add('ativo');
-        var rating = e.target.getAttribute('data-avaliacao');
-        console.log(rating);
-        submitButton.style.display = 'block';
-    }
-});
+    });
 
-submitButton.addEventListener('click', function() {
-    var activeStar = document.querySelector('.star-icon.ativo');
-    var rating = activeStar.getAttribute('data-avaliacao');
-    alert('Avaliação enviada: ' + rating);
-    // Aqui você pode adicionar o código para enviar a avaliação para o servidor
+    submitButton.addEventListener('click', function() {
+        var activeStars = document.querySelectorAll('.star-icon.ativo');
+        if (activeStars.length > 0) {
+            avaliacao.classList.add('avaliacao-enviada');
+            submitButton.style.display = 'none';
+            btnAvaliar.textContent = 'Excluir';
+            stars.forEach(star => {
+                star.style.pointerEvents = 'none';
+            });
+        }
+    });
+
+    function paintStars(rating) {
+        stars.forEach((s, index) => {
+            if (index < rating) {
+                s.classList.add('ativo');
+            } else {
+                s.classList.remove('ativo');
+            }
+        });
+    }
 });
