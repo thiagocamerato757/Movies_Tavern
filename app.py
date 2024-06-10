47de52c3306cc8844242482463530500ab1bee55
Ana Luiza <aninhapmarques03@gmail.com>
2024-06-10 12:48:25 -0300

    alteracao da paginacao, ordenacao do filme e botao do movies tavern

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import re
 
 app = Flask(__name__)
 
@@ -59,17 +60,16 @@ def search():
     page = request.args.get('page', 1, type=int) 
     if query:
         # Faz uma requisição GET à API do TMDb para buscar filmes
-        url = f'https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={query}&page={page}'
+        url = f'https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={query}&page={page}&sort_by=vote_average.desc'
         response = requests.get(url)
         data = response.json() # Converte a resposta em JSON
         movies = data.get('results', []) # Obtém a lista de resultados
-        total_pages = data.get('total_pages', 1) 
+        sorted_movies = sorted(movies, key=lambda x: x['title']) #ordenando filmes pelo título
+        total_pages = data.get('total_pages', 1)
     else:
         movies = [] # Se não houver termo de busca, a lista de filmes é vazia
         total_pages = 1
-    return render_template('search.html', movies=movies,query=query, page=page, total_pages=total_pages)
-
-
+    return render_template('search.html', movies=sorted_movies,query=query, page=page, total_pages=total_pages)
 
 @app.route('/movie/<int:movie_id>')
 def movie_detail(movie_id):
@@ -87,6 +87,27 @@ def movie_detail(movie_id):
     response = requests.get(url)
     movie = response.json() # Converte a resposta em JSON
     return render_template('movie.html', movie=movie) # Renderiza o template com os detalhes do filme
+
+def pagination_range(current_page, total_pages, delta=1):
+    """
+    Generate pagination range for displaying limited pages with '...' in between.
+    
+    :param current_page: The current active page.
+    :param total_pages: The total number of pages.
+    :param delta: The number of pages to display around the current page.
+    :return: A list with the pagination range and '...' as None.
+    """
+    range_with_dots = []
+    for p in range(1, total_pages + 1):
+        if p == 1 or p == total_pages or (current_page - delta <= p <= current_page + delta):
+            range_with_dots.append(p)
+        elif range_with_dots[-1] is not None:
+            range_with_dots.append(None)
+    return range_with_dots
+
+@app.context_processor
+def utility_processor():
+    return dict(pagination_range=pagination_range)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001) # Executa o aplicativo Flask no modo debug
